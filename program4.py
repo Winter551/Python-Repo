4231806d8437136c67117551b7cfaf396b7174ba
Winter551 <143036713+Winter551@users.noreply.github.com>
2023-08-30 19:42:53 -0500

    Added code

--- program4.py
+++ program4.py
@@ -1,1 +1,15 @@
-print("Hello")
+names = []
+code = []
+
+with open ("data.csv","r") as countries_csv:
+    lines = countries_csv.readlines()
+
+    for i in lines:
+        new_list = i.split(",")
+        names.append(new_list[0])
+        code.append(new_list[1])
+
+    for i in range(len(names)):
+        print("-----------")
+        print("Name: " + names[i])
+        print("Code: " + code[i])
