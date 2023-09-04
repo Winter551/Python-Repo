336da83e63ff967808fa86f7707dce673957de62
Winter551 <143036713+Winter551@users.noreply.github.com>
2023-09-04 12:41:45 -0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,7 @@
+
+#Ask the user for their name and birthday
+name = input("What is your name? ")
+bday = input("What is your Birthday? ")
+
+#Print happy bday statment with name and bday in f string
+print(f"Omg it's {bday}! Happy Birthday {name}!!!")
