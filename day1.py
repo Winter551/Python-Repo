a2b3db3934bb85f69d837ece7bbd86229eaf6db3
Winter551 <143036713+Winter551@users.noreply.github.com>
2023-09-04 17:38:37 -0500

    Add misc

--- day1.py
+++
@@ -1,7 +0,0 @@
-
-#Ask the user for their name and birthday
-name = input("What is your name? ")
-bday = input("What is your Birthday? ")
-
-#Print happy bday statment with name and bday in f string
-print(f"Omg it's {bday}! Happy Birthday {name}!!!")
