5d743d35c319c5c193859618f5b2f53f10705bbc
Winter551 <143036713+Winter551@users.noreply.github.com>
2023-09-27 15:49:36 -0500

    Update Calculator.py

--- Challenges/Calculator.py
+++ Challenges/Calculator.py
@@ -10,3 +10,18 @@ if z == ("+"):
     print(int(x) + int(y))
 if z == ("-"):
     print(int(x) - int(y))
+
+
+def xor_decrypt(ciphertext, key):
+    plaintext = ""
+    for char in ciphertext:
+        decrypted_char = chr(ord(char) ^ key)
+        plaintext += decrypted_char
+    return plaintext
+
+ciphertext = "$/8#$!%&&$:;/.8\"%.%#%#"
+
+print("Brute-forcing 1-byte XOR decryption:")
+for key in range(256):
+    decrypted_text = xor_decrypt(ciphertext, key)
+    print(f"Key: {key:02X} - Decrypted Text: {decrypted_text}")
